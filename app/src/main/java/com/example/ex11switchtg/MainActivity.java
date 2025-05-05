cf0962498c835d4e1cfc45805ad8c0badcbf44eb
ng5134 <ng5134@bs.amalnet.k12.il>
2025-05-05 23:59:14 +0300

    גאווה 2

--- app/src/main/java/com/example/ex11switchtg/MainActivity.java
+++ app/src/main/java/com/example/ex11switchtg/MainActivity.java
@@ -2,14 +2,15 @@ package com.example.ex11switchtg;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private ConstraintLayout mainLayout;
     private RadioGroup radioGroup;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         radioGroup = findViewById(R.id.radioGroup);
         btnChange = findViewById(R.id.btnChange);
         mySwitch = findViewById(R.id.mySwitch);
+
         // Case 1: If the switch is ON, background changes immediately on radio button selection
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -36,14 +38,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Case 2: If the switch is OFF, background changes only when button is clicked
-        btnChange.setOnClickListener(new android.view.View.OnClickListener() {
+        btnChange.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(android.view.View v) {
+            public void onClick(View v) {
                 if (!mySwitch.isChecked()) {
                     int selectedId = radioGroup.getCheckedRadioButtonId();
                     changeBackgroundColor(selectedId);
                 }
             }
         });
+    }
+
+    //changes the background color based on selected radio button
+    private void changeBackgroundColor(int radioId) {
+        if (radioId == R.id.radioRed) {
+            mainLayout.setBackgroundColor(Color.RED);
+        }
+        else if (radioId == R.id.radioBlue) {
+            mainLayout.setBackgroundColor(Color.BLUE);
+        }
+        else if (radioId == R.id.radioGreen) {
+            mainLayout.setBackgroundColor(Color.GREEN);
+        }
+        else if (radioId == R.id.radioYellow) {
+            mainLayout.setBackgroundColor(Color.YELLOW);
+        }
     }
 }
